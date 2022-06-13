387d137e8ef8bf570362d6f66f745c8fb3b551e1
Tsapiv Volodymyr <volodja.tsv@gmail.com>
2022-06-13 20:58:38 +0300

    Add summary

--- src/knn.py
+++ src/knn.py
@@ -6,7 +6,7 @@ from training import select_model
 if __name__ == '__main__':
     digit1, digit2 = 8, 5
     dataset = load(digit1, digit2)
-    parameters = {'n_neighbors': list(range(10, 30, 2))}
+    parameters = {'n_neighbors': list(range(11, 30, 2))}
     select_model(dataset, KNeighborsClassifier, parameters)
 
 
